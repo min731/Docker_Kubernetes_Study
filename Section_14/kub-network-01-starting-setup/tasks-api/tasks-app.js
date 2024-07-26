1f7baafd489aebf87cd38e8d995c2f60867db066
min0731 <ypd04208@gmail.com>
2024-07-26 21:14:44 +0900

    K8S_namespace_CoreDNS_FE_reverse_proxy

--- Section_14/kub-network-01-starting-setup/tasks-api/tasks-app.js
+++ Section_14/kub-network-01-starting-setup/tasks-api/tasks-app.js
@@ -11,13 +11,26 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// 로컬 frontend 어플리케이션에서 브라우저가 task-api에 접근할 수 있도록 헤더 수정
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+})
+
 const extractAndVerifyToken = async (headers) => {
   if (!headers.authorization) {
     throw new Error('No token provided.');
   }
   const token = headers.authorization.split(' ')[1]; // expects Bearer TOKEN
 
-  const response = await axios.get('http://auth/verify-token/' + token);
+  // 쿠버네티스 상에서 타 Pod와 통신하기 위해 변경
+  // const response = await axios.get('http://auth/verify-token/' + token);
+  // (1) docker-compose 환경변수 이용
+  const response = await axios.get(`http://${process.env.AUTH_ADDRESS}/verify-token/` + token);
+  // (2) K8S ClusterIP 활용, (3) K8S CoreDNS(서비스명.네임스페이스명) 활용
+
   return response.data.uid;
 };
 
